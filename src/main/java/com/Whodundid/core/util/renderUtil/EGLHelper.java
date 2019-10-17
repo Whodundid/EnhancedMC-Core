52855e8f108b04cc607d178fabaa05389b0483cc
Hunter Bragg <Whodundid@gmail.com>
2019-10-17 09:28:39 -0600

    A class containing additional opengl helper methods.

---
+++ src/main/java/com/Whodundid/core/util/renderUtil/EGLHelper.java
@@ -0,0 +1,15 @@
+package com.Whodundid.core.util.renderUtil;
+
+import net.minecraft.client.renderer.GlStateManager;
+
+public class EGLHelper {
+	
+	public static void setColor(int colorIn) {
+		float f3 = (colorIn >> 24 & 255) / 255.0F;
+        float f = (colorIn >> 16 & 255) / 255.0F;
+        float f1 = (colorIn >> 8 & 255) / 255.0F;
+        float f2 = (colorIn & 255) / 255.0F;
+        GlStateManager.color(f, f1, f2, f3);
+	}
+
+}
