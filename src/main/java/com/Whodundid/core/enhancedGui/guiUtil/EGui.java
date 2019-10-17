b29f11e852c4383d666c30959b87158c931f2481
Hunter Bragg <Whodundid@gmail.com>
2019-10-17 09:30:32 -0600

    Added circle, and ellipse drawing.

--- src/main/java/com/Whodundid/core/enhancedGui/guiUtil/EGui.java
+++ src/main/java/com/Whodundid/core/enhancedGui/guiUtil/EGui.java
@@ -2,7 +2,7 @@ package com.Whodundid.core.enhancedGui.guiUtil;
 
 import com.Whodundid.core.EnhancedMC;
 import com.Whodundid.core.util.miscUtil.EFontRenderer;
-
+import com.Whodundid.core.util.renderUtil.EGLHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 //Last edited: Jan 3, 2019
 //First Added: Dec 18, 2018
@@ -53,6 +54,82 @@ public class EGui {
         drawRect(x, startY + 1, x + 1, endY, color);
     }
     
+    public static void drawCircle(double posX, double posY, double radius, int detail, int color) {
+    	GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        EGLHelper.setColor(color);
+        GL11.glBegin(GL11.GL_LINE_LOOP);
+        for (int i = 0; i < detail; i++) {
+        	double theta = 2.0f * Math.PI * i / detail;
+        	double x = radius * Math.cos(theta);
+        	double y = radius * Math.sin(theta);
+        	GL11.glVertex2d(x + posX, y + posY);
+        }
+        GL11.glEnd();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+    
+    public static void drawFilledCircle(double posX, double posY, double radius, int detail, int color) {
+    	GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        EGLHelper.setColor(color);
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+        double prevX = 0.0, prevY = 0.0;
+        GL11.glVertex2d(posX, posY);
+        for (int i = 0; i < detail + 1; i++) {
+        	double theta = 2.0f * Math.PI * i / detail;
+        	double x = radius * Math.cos(theta);
+        	double y = radius * Math.sin(theta);
+        	GL11.glVertex2d(x + posX, y + posY);
+        	GL11.glVertex2d(posX + prevX, posY + prevY);
+        	prevX = x; prevY = y;
+        }
+        GL11.glEnd();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+    
+    public static void drawElipse(double posX, double posY, double radiusX, double radiusY, int detail, int color) {
+    	GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        EGLHelper.setColor(color);
+        GL11.glBegin(GL11.GL_LINE_LOOP);
+        for (int i = 0; i < detail + 1; i++) {
+        	double theta = 2.0f * Math.PI * i / detail;
+        	double x = radiusX * Math.cos(theta);
+        	double y = radiusY * Math.sin(theta);
+        	GL11.glVertex2d(x + posX, y + posY);
+        }
+        GL11.glEnd();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+    
+    public static void drawFilledElipse(double posX, double posY, double radiusX, double radiusY, int detail, int color) {
+    	GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        EGLHelper.setColor(color);
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+        double prevX = 0.0, prevY = 0.0;
+        GL11.glVertex2d(posX, posY);
+        for (int i = 0; i < detail + 1; i++) {
+        	double theta = 2.0f * Math.PI * i / detail;
+        	double x = radiusX * Math.cos(theta);
+        	double y = radiusY * Math.sin(theta);
+        	GL11.glVertex2d(x + posX, y + posY);
+        	GL11.glVertex2d(posX + prevX, posY + prevY);
+        	prevX = x; prevY = y;
+        }
+        GL11.glEnd();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+    
     public static void drawRect(int left, int top, int right, int bottom, int color) {
         if (left < right) {
             int i = left;
@@ -65,17 +142,13 @@ public class EGui {
             top = bottom;
             bottom = j;
         }
-
-        float f3 = (color >> 24 & 255) / 255.0F;
-        float f = (color >> 16 & 255) / 255.0F;
-        float f1 = (color >> 8 & 255) / 255.0F;
-        float f2 = (color & 255) / 255.0F;
+        
         Tessellator tessellator = Tessellator.getInstance();
         WorldRenderer worldrenderer = tessellator.getWorldRenderer();
         GlStateManager.enableBlend();
         GlStateManager.disableTexture2D();
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        GlStateManager.color(f, f1, f2, f3);
+        EGLHelper.setColor(color);
         worldrenderer.begin(7, DefaultVertexFormats.POSITION);
         worldrenderer.pos(left, bottom, 0.0D).endVertex();
         worldrenderer.pos(right, bottom, 0.0D).endVertex();
