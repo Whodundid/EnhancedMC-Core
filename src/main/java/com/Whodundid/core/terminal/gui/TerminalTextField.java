5d5ea789911f687f2adbb6864ae8c54db820b23b
Hunter Bragg <Whodundid@gmail.com>
2019-12-30 15:58:42 -0700

    Made Terminal history globally static.

--- src/main/java/com/Whodundid/core/terminal/gui/TerminalTextField.java
+++ src/main/java/com/Whodundid/core/terminal/gui/TerminalTextField.java
@@ -1,16 +1,20 @@
 package com.Whodundid.core.terminal.gui;
 
+import com.Whodundid.core.EnhancedMC;
 import com.Whodundid.core.enhancedGui.guiObjects.basicObjects.EGuiTextField;
+import com.Whodundid.core.terminal.TerminalCommandHandler;
 import com.Whodundid.core.util.storageUtil.EArrayList;
 
 public class TerminalTextField extends EGuiTextField {
 
+	protected TerminalCommandHandler handler;
 	protected ETerminal term;
 	protected boolean isTabCompleting = false;
 	
 	public TerminalTextField(ETerminal termIn, int xIn, int yIn, int widthIn, int heightIn) {
 		super(termIn, xIn, yIn, widthIn, heightIn);
 		term = termIn;
+		handler = EnhancedMC.getTerminalHandler();
 		setUseObjectGroupForCursorDraws(true);
 		setDrawShadowed(false);
 	}
@@ -27,7 +31,7 @@ public class TerminalTextField extends EGuiTextField {
 			term.preservedInput = "";
 		}
 		if (keyCode == 200) { //up
-			if (term.historyLine < term.cmdHistory.size()) {
+			if (term.historyLine < handler.cmdHistory.size()) {
 				if (term.lastUsed == 0) {
 					term.historyLine += 1;
 					setText(getHistoryLine(term.historyLine));
@@ -40,8 +44,8 @@ public class TerminalTextField extends EGuiTextField {
 			term.lastUsed = 1;
 		}
 		else if (keyCode == 208) { //down
-			if (term.historyLine <= term.cmdHistory.size() && term.historyLine > 1) {
-				if (term.historyLine == term.cmdHistory.size() || term.lastUsed == 1) { term.historyLine -= 2; }
+			if (term.historyLine <= handler.cmdHistory.size() && term.historyLine > 1) {
+				if (term.historyLine == handler.cmdHistory.size() || term.lastUsed == 1) { term.historyLine -= 2; }
 				else { term.historyLine -= 1; }
 				setText(getHistoryLine(term.historyLine));
 				term.lastUsed = 0;
@@ -57,9 +61,9 @@ public class TerminalTextField extends EGuiTextField {
 	}
 	
 	private String getHistoryLine(int lineNum) {
-		if (!term.cmdHistory.isEmpty()) {
-			if (lineNum == 0) { return term.cmdHistory.get(term.cmdHistory.size() - 1); }
-			else if (lineNum > 0) { return term.cmdHistory.get(term.cmdHistory.size() - (lineNum + 1)); }
+		if (!handler.cmdHistory.isEmpty()) {
+			if (lineNum == 0) { return handler.cmdHistory.get(handler.cmdHistory.size() - 1); }
+			else if (lineNum > 0) { return handler.cmdHistory.get(handler.cmdHistory.size() - (lineNum + 1)); }
 		}
 		return null;
 	}
